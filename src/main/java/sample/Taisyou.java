c46ff491eda649cb4280150e79bc0bcbe4080710
ittimfn <ittimfn+github@gmail.com>
2021-12-27 11:37:23 +0900

    コメント追加

--- src/main/java/sample/Taisyou.java
+++ src/main/java/sample/Taisyou.java
@@ -8,18 +8,30 @@ public class Taisyou {
         return util.get();
     }
     
+    /**
+     * 自分自身のprivateメソッドを呼ぶ。コレ自体はpublic。
+     */
     public String callPrivatePrint() {
         return print();
     }
     
+    /**
+     * 自分自身のprivateメソッドを呼ぶ。コレ自体もprivate。
+     */
     private String callPrivatePrint_meToo() {
         return print();
     }
 
+    /**
+     * privateメソッド。あちこちから呼ばれる。
+     */
     private String print() {
         return "いやいやそんなはずは";
     }
     
+    /**
+     * public staticメソッド。
+     */
     public static String staticMethod() {
         return "staticメソッドのMockができていない";
     }
